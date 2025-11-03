570892690118967f8c6516fd1ef6d74010c6704f
Guilherme Souza Ferrari <guilhermesouzaferrari@gmail.com>
2025-11-03 16:19:18 -0300

    Commit inicial do projeto Doxsys

---
+++ credenciais.py
@@ -0,0 +1,18 @@
+doxenv = 'production'
+dbname = 'resellercontrol'
+dbusr = 'doxsys'
+dbpwd = 'DOX*doxsys2023'
+send_smtp_srv = "smtp.gmail.com"
+sender_email = "admin@doxsg.com"
+sender_password = "idom vkjq vclu ftwd"
+# O ZAPI_CLIENT é o mesmo para todas as intâncias
+ZAPI_CLIENT='F61a504f2582f472ca7cf53f0839e416dS'
+# Instância=Suporte11978109256
+ZAPI_INSTANCE_SUPP='3E16EBDA683A1067961A16374B74BED7'
+ZAPI_TOKEN_SUPP='6D690B9EBD02672884E7317D'
+# Instância=Sales11970847098
+ZAPI_INSTANCE_SALES='3E1C94A24BB9F04DDAD9826401C24A0E'
+ZAPI_TOKEN_SALES='47E03B5E68DCA7BE1A602A1D'
+#Synology User
+SYNOLOGY_USR='backup'
+SYNOLOGY_PWD='Dox#2011'
